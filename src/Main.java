2f65d725d8929ae7c89d006b5d5b24949ac1b2a4
Soulike <316413310@qq.com>
2018-07-24 19:52:52 +0800

    细化错误检查

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,45 @@ public class Main
 {
     public static void main(String[] args)
     {
-        if (args.length != 3)
+        try
         {
-            System.out.println("参数数量错误");
-            System.exit(0);
-        }
-        final String nodeId = args[0];
-        final int port = Integer.parseInt(args[1]);
-        final Path neighborConfigFilePath = Paths.get(args[2]);
-
-        final Properties properties = new Properties();
-        try (InputStream in = new FileInputStream("config.conf"))
-        {
-            properties.load(in);
-            final long heartBeatSendInterval = (long) Double.parseDouble(properties.getProperty("heartBeatSendInterval"));
-            final long graphInfoSendInterval = (long) (Double.parseDouble(properties.getProperty("graphInfoSendInterval")) * 1000);
-            final long printInterval = (long) (Double.parseDouble(properties.getProperty("printInterval")) * 1000);
-            try
+            if (args.length != 3)
             {
-                Node node = new Node(nodeId, port, neighborConfigFilePath, heartBeatSendInterval, graphInfoSendInterval, printInterval);
-                node.listen();
+                throw new Exception("命令行参数数量错误");
             }
-            catch (IOException e)
+
+            final String nodeId = args[0];
+            final int port = Integer.parseInt(args[1]);
+            final Path neighborConfigFilePath = Paths.get(args[2]);
+
+            final Properties properties = new Properties();
+            try (InputStream in = new FileInputStream("config.conf"))
             {
-                System.out.println("结点邻居结点配置文件不存在或网络连接错误");
+                properties.load(in);
+                final long heartBeatSendInterval = (long) Double.parseDouble(properties.getProperty("heartBeatSendInterval"));
+                final long graphInfoSendInterval = (long) (Double.parseDouble(properties.getProperty("graphInfoSendInterval")) * 1000);
+                final long printInterval = (long) (Double.parseDouble(properties.getProperty("printInterval")) * 1000);
+                try
+                {
+                    Node node = new Node(nodeId, port, neighborConfigFilePath, heartBeatSendInterval, graphInfoSendInterval, printInterval);
+                    node.listen();
+                }
+                catch (IOException e)
+                {
+                    System.out.println("结点邻居结点配置文件不存在或网络连接错误");
+                    System.out.println(e.getLocalizedMessage());
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("配置文件读取错误");
                 System.out.println(e.getLocalizedMessage());
             }
         }
-        catch (FileNotFoundException e)
+        catch (Exception e)
         {
-            System.out.println("配置文件不存在");
-            System.out.println(e.getLocalizedMessage());
-        }
-        catch (IOException e)
-        {
-            System.out.println("配置文件读取错误");
-            System.out.println(e.getLocalizedMessage());
+            System.err.println("命令行参数无效");
+            System.err.println(e.getLocalizedMessage());
         }
     }
 }
